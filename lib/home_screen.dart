8f398b8183a68e26e4890c67fde00fd9be0865af
Aryak <221111535@students.mikroskil.ac.id>
2024-05-28 18:50:19 +0700

    update tampilan home

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:project_uas/style.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,27 +14,84 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(221.0),
-          child: AppBar(
-            title: Text(
-              'Selamat\nDatang',
-              style: TextStyles.titleApp,
-            ),
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/appbar.png"),
-                    fit: BoxFit.fill),
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        onTap: (index) {},
+        animationDuration: const Duration(milliseconds: 200),
+        backgroundColor: AppColors.putihCerah,
+        color: AppColors.hijauGelap,
+        items: const [
+          Icon(
+            Icons.dehaze,
+            color: AppColors.putihCerah,
+          ),
+          Icon(
+            Icons.access_alarm,
+            color: AppColors.putihCerah,
+          ),
+          Icon(
+            Icons.settings,
+            color: AppColors.putihCerah,
+          ),
+        ],
+      ),
+      body: SafeArea(
+          child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            child: Container(
+              height: 221,
+              width: double.infinity,
+              color: const Color.fromARGB(255, 245, 247, 247),
+              child: Image.asset(
+                "assets/images/appbar.png",
+                fit: BoxFit.cover,
               ),
             ),
           ),
-        ),
-        // body: ,
-      ),
+          Positioned(
+            bottom: 0,
+            left: 30,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 2,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      width: 150,
+                      height: 140,
+                      child: Text("Selamat\nDatang",
+                          style: TextStyles.titleApp
+                              .copyWith(color: AppColors.putihCerah)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 30,
+            bottom: 0,
+            child: Expanded(
+              child: Column(
+                children: [
+                  SizedBox(
+                    child: Text(
+                      "Obat Hari Ini",
+                      style: TextStyles.titleForm,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
